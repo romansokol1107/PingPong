84d95cec609cedae4b09268c8c51ed44bc4f6501
romansokol1107 <88234703+romansokol1107@users.noreply.github.com>
2024-01-06 12:24:43 +0300

    Total version 1.0
    The final version of Ping Pong game for 2 players

--- enemy.py
+++ enemy.py
@@ -5,12 +5,11 @@ import gamesprite
 
 
 class Enemy(gamesprite.GameSprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, width, height, player_lost):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height, player_speed_x):
         super().__init__(player_image, player_x, player_y, player_speed, width, height)
-        self.lost = player_lost
+        self.speed_x = player_speed_x
     def update(self):
         self.rect.y += self.speed
-        if self.rect.y >= HEIGHT:
-            self.lost += 1
-            self.rect.y = 0
-            self.rect.x = randint(0, WIDTH - self.width)
+        self.rect.x += self.speed_x
+        if self.rect.bottom >= HEIGHT or self.rect.y <= 0:
+            self.speed *= -1
--- pingpong.py
+++ pingpong.py
@@ -9,10 +9,12 @@ from time import time as timer
 
 font.init()
 
+
 window = display.set_mode((WIDTH,HEIGHT))
 display.set_caption('Ping Pong')
 player_l = player.Player('platform.png', 0, HEIGHT//2, 5, WIDTH_PLATFORM, 65)
 player_r = player.Player('platform.png', WIDTH-WIDTH_PLATFORM, HEIGHT//2, 5, WIDTH_PLATFORM, 65)
+ball = Enemy('ball.png', WIDTH//2, HEIGHT//2, 3, 20, 20, 3)
 
 font_style = font.SysFont('Arial', 14)
 font_style2 = font.SysFont('Times New Roman', 70)
@@ -31,9 +33,12 @@ while game:
         player_r.update_r()
         player_l.reset(window)
         player_r.reset(window)
-        '''
         ball.update()
         ball.reset(window)
-        '''
+        if sprite.collide_rect(player_l, ball):
+            ball.speed_x *= -1
+        if sprite.collide_rect(player_r, ball):
+            ball.speed_x *= -1
+        if 
     display.update()
     clock.tick(FPS)
